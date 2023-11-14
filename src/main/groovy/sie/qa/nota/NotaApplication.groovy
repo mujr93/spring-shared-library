67c2392ef29992808ffae91c93e5e686ec01b358
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-13 23:23:04 -0600

    Add files via upload

---
+++ src/main/groovy/sie/qa/nota/NotaApplication.groovy
@@ -0,0 +1,13 @@
+package sie.qa.nota
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NotaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(NotaApplication, args)
+	}
+
+}
